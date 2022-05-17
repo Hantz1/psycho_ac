f3ff874c9eec9e9a3c465756cbaf174c345505b4
Hantz1 <jeffhantzbelizaire604@gmail.com>
2022-05-17 11:26:11 -0400

    mise en place index

--- index.php
+++ index.php
@@ -4,7 +4,41 @@
         <title>Psycho_AC</title>
         <meta charset="utf-8">
     </head>
+
     <body>
-        
+        <header>
+            <div class="wrapper">
+                <?php include menu.php?>
+            </div>
+        </header>
+
+        <section>
+            <div class="wrapper">
+            </div>
+        </section>
+
+        <section>
+            <div class="wrapper">
+            </div>
+        </section>
+
+        <section>
+            <div class="wrapper">
+            </div>
+        </section>
+
+        <section>
+            <div class="wrapper">
+            </div>
+        </section>
+
+        <section>
+            <div class="wrapper">
+            </div>
+        </section>
+
+        <footer>
+
+        </footer>
     </body>
 </html>
