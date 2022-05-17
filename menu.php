6f8c1ae45717757ed7e534a75f12214ee04d0daf
Hantz1 <jeffhantzbelizaire604@gmail.com>
2022-05-17 11:26:39 -0400

    mise en place menu

--- menu.php
+++ menu.php
@@ -1,9 +1,14 @@
 <div>
-    <ul>
-        <li><a>Accueil</a></li>
-        <li><a></a></li>
-        <li><a></a></li>
-        <li><a></a></li>
-        <li><a></a></li>
-    </ul>
+    <h1>Psycho AC<span>.</span></h1>
+    <nav>
+        <ul>
+            <li><a>Accueil</a></li>
+            <li><a>Services</a></li>
+            <li><a>Support group</a></li>
+            <li><a>Consultation</a></li>
+            <li><a>Forum</a></li>
+            <li><a>Sign in</a></li>
+        </ul>
+    </nav>
+    
 </div>
